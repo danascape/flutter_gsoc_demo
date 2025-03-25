3beb92592eee737a96d7f258066f2d9b85d641ed
Saalim Quadri <danascape@gmail.com>
2025-03-25 06:58:31 +0530

    Load default image
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Flutter GSoC Demo'),
+      home: const MyHomePage(title: 'Flutter Demo'),
     );
   }
 }
@@ -23,39 +20,19 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _showImage = false;
-  bool _isConnected = false;
 
-
-void _checkConnectivity() async {
-  var connectivityResult = await Connectivity().checkConnectivity();
-  bool hasInternet = false;
-
-  if (connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi) {
-    hasInternet = true;
-  } else {
-    // Uhmm for some reason wired connection was not working on QEMU.
-    try {
-      final result = await InternetAddress.lookup('google.com');
-      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-        hasInternet = true;
-      }
-    } catch (e) {
-      hasInternet = false;
-    }
+  void _loadImage() {
+    setState(() {
+      _showImage = true;
+    });
   }
-
-  setState(() {
-    _isConnected = hasInternet;
-    _showImage = hasInternet;
-  });
-}
 
   @override
   Widget build(BuildContext context) {
@@ -65,25 +42,23 @@ void _checkConnectivity() async {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Saalim Quadri',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _checkConnectivity,
-              child: Text('Show Image'),
+              onPressed: _loadImage,
+              child: const Text('Load Image'),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             _showImage
-                ? Image.network(
-                    'https://picsum.photos/200',
+                ? Image.asset(
+                    'assets/images/saalim.jpeg',
                     width: 150,
                     height: 150,
                   )
-                : _isConnected
-                    ? Container()
-                    : Text("No Internet Connection", style: TextStyle(color: Colors.red)),
+                : Container(),
           ],
         ),
       ),
